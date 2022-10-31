0c88c0ca5456fd3488fae7cf486c1db06194b479
cseMotaleb <csemotaleb@gmail.com>
2022-11-01 00:03:17 +0600

    menu development

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,16 +4,19 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div>
-      <Navbar bg="light" expand="lg">
+    <div className="header">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
         <Container>
           <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="m-auto">
               <NavItem> <Link className="nav-link" to="/">Home</Link> </NavItem>
               <NavItem> <Link className="nav-link" to="/about">About</Link> </NavItem>
               <NavItem> <Link className="nav-link" to="/contact">Contact</Link> </NavItem>
+            </Nav>
+            <Nav>
+              <Nav.Link to="#deets">Hire Me</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
