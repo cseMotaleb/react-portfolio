092241d75e9ab266c3ac805a43ef816d4a5118a1
cseMotaleb <csemotaleb@gmail.com>
2022-11-15 11:42:37 +0600

    All page component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
+import Portfolio from "./pages/portfolio/Portfolio";
 import Service from "./pages/service/Service";
+import Resume from "./pages/resume/Resume";
+import Blog from "./pages/blog/Blog";
 import Contact from "./pages/contact/Contact";
 
 
@@ -14,7 +17,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/service" element={<Service />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/blog" element={<Blog />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,9 +2,6 @@ import { Container, Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
-
-
-
 function Header() {
   return (
     <header className="header">
@@ -17,11 +14,11 @@ function Header() {
               <NavItem> <Link className="nav-link" exact
                 to="/">HOME</Link> </NavItem>
               <NavItem> <Link className="nav-link" to="/about">ABOUT</Link> </NavItem>
-              <NavItem> <Link className="nav-link" to="/resume">RESUME</Link> </NavItem>
               <NavItem> <Link className="nav-link" to="/portfolio">PORTFOLIO</Link> </NavItem>
+              <NavItem> <Link className="nav-link" to="/resume">RESUME</Link> </NavItem>
+              <NavItem> <Link className="nav-link" to="/service">SERVICE</Link> </NavItem>
               <NavItem> <Link className="nav-link" to="/blog">BLOG</Link> </NavItem>
               <NavItem> <Link className="nav-link" to="/contact">CONTACT</Link> </NavItem>
-              
             </Nav>
             <Nav>
               <Nav.Link to="#deets">Hire Me</Nav.Link>
